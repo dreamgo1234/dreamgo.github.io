a3ee2795229f6cc372826a0ea2d3a28c6bdb8ee9
dreamgo1234 <134962921+dreamgo1234@users.noreply.github.com>
2023-05-29 23:41:13 +0530

    dreamgo

---
+++ book_form.php
@@ -0,0 +1,23 @@
+<?php
+
+$connection = mysqli_connect('localhost','root','','book_database');
+
+if(isset($_POST['Send'])){ 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+   $location = $_POST['location'];
+   $guests = $_POST['guests'];
+   $arrivals = $_POST['arrivals'];
+   $leaving = $_POST['levaing'];
+
+   $request = "insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values ('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving')";
+
+   mysqli_query($connection, $request);
+
+   header('location:book.php');
+}else{
+    echo 'something went wrong try again';
+}
+?>
---
+++ package.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-eqiv="x-ua-compatible" content=-IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>package</title>
+    <!-- swiper css link -->
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file  -->
+
+    <link rel="stylesheet" href="css\style.css">
+</head>
+
+<body>
+
+    <!-- header section starts -->
+
+    <section class="header">
+        <a href="home.php" class="logo">Dream Go travel</a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+     <!-- header section ends -->
+
+     <div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+
+        <h3>package</h3>
+
+    </div>
+
+
+
+
+
+
+
+
+<!-- packages section starts -->
+
+<section class="packages">
+
+<h1 class="heading-title">top destinations</h1>
+
+<div class="box-container">
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-1.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-2.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-3.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-4.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-5.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-6.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+            <img src="images/img-7.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-8.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-9.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-10.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-11.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div class="image">
+
+            <img src="images/img-12.jpg" alt="">
+
+        </div>
+
+        <div class="contant">
+
+            <h3>adventure & tour</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime!</p>
+            <a href="book.php" class="btn">book now</a>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+<div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- footer section starts -->
+
+    <section class="footer">
+
+
+
+        <div class="box-container">
+
+            <div class="box">
+
+                <h3>Quick links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+
+            </div>
+
+            <div class="box">
+
+                <h3>Extra Links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> About us </a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy Policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+
+            </div>
+
+            <div class="box">
+
+                <h3>Contact Info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +123-956-8977</a>
+                <a href="#"> <i class="fas fa-phone"></i> +111-955-8977</a>
+                <a href="#"> <i class="fas fa-envelope"></i> # gamail id dalni h </a>
+                <a href="#"> <i class="fas fa-map"></i> Kanpur,U.P 208017</a>
+
+            </div>
+
+            <div class="box">
+
+                <h3>Follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> Facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+
+            </div>
+
+        </div>
+
+        <div class="credit">created by<span>mr.web designer</span>|all rights are reserved!</div>
+
+    </section>
+
+    <!-- footer section end -->
+
+
+
+
+    <!-- swiper js link -->
+
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link -->
+
+    <script src="js\script.js"></Script>
+
+
+
+</body>
+
+</html>
